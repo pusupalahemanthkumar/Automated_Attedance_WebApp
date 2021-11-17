42d26710734cbd9104779a0366a239f6286a9c8a
Hemanth Kumar <pusupalahemanthkumar@gmail.com>
2021-11-17 21:57:57 +0530

    added pwa installability faeture

---
+++ client/public/serviceworker.js
@@ -0,0 +1,15 @@
+self.addEventListener("install", (event) => {
+    console.log("[Service Wroker ] Installing Service Worker..");
+    console.log(event);
+  });
+  
+  self.addEventListener("activate", (event) => {
+    console.log("[Service Wroker ] Activating Service Worker..");
+    console.log(event);
+    return self.clients.claim();
+  });
+  
+  self.addEventListener("fetch", (event) => {
+    console.log("[Service Wroker ] Fetching ...", event);
+    event.respondWith(fetch(event.request));
+  });
