6c62f1ff3af7f12cdc26d81b4884a2704adef40e
Hemanth Kumar <pusupalahemanthkumar@gmail.com>
2022-03-29 10:59:15 +0530

    added basic code for voice_assistant

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React ,{useEffect} from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import alanBtn from "@alan-ai/alan-sdk-web";
 
 import LoginPage from "./Screens/LoginPage";
 import RegisterPage from "./Screens/RegisterPage";
@@ -8,8 +9,20 @@ import TakeAttendancePage from './Screens/TakeAttendancePage';
 import DashboardPage from './Screens/DashboardPage';
 import AdminPage from "./Screens/AdminPage"
 
+const ALAN_KEY="032cb63c95e4a228e2cefa5e29d42e1b2e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 const App = () => {
+  useEffect(()=>{
+    alanBtn({
+      key:ALAN_KEY,
+      onCommand:({command})=>{
+        if(command==="test"){
+          alert("testing");
+        }
+      }
+    })
+
+  },[])
   return (
     <Router>
       <Route path="/" exact component={HomePage} />
