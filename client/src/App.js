50e856150986d80e2fe1a3a8ef3b9c3b4f44623b
Hemanth Kumar <pusupalahemanthkumar@gmail.com>
2022-03-31 22:15:42 +0530

    added code voice navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,49 @@
-import React ,{useEffect} from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import {Route, useHistory, Switch } from "react-router-dom";
 import alanBtn from "@alan-ai/alan-sdk-web";
 
 import LoginPage from "./Screens/LoginPage";
 import RegisterPage from "./Screens/RegisterPage";
-import HomePage from './Screens/HomePage';
-import TakeAttendancePage from './Screens/TakeAttendancePage';
-import DashboardPage from './Screens/DashboardPage';
-import AdminPage from "./Screens/AdminPage"
+import HomePage from "./Screens/HomePage";
+import TakeAttendancePage from "./Screens/TakeAttendancePage";
+import DashboardPage from "./Screens/DashboardPage";
+import AdminPage from "./Screens/AdminPage";
 
-const ALAN_KEY="032cb63c95e4a228e2cefa5e29d42e1b2e956eca572e1d8b807a3e2338fdd0dc/stage";
+const ALAN_KEY =
+  "032cb63c95e4a228e2cefa5e29d42e1b2e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 const App = () => {
-  useEffect(()=>{
+  let history = useHistory();
+  useEffect(() => {
     alanBtn({
-      key:ALAN_KEY,
-      onCommand:({command})=>{
-        if(command==="test"){
-          alert("testing");
+      key: ALAN_KEY,
+      onCommand: ({ command, pageName }) => {
+        console.log(pageName);
+        if (command === "home") {
+          history.push('/');
+        }else if(command==="dashboard"){
+          history.push("/dashboard");
+        }else if(command==="admin"){
+          history.push("/admin");
+        }else if(command==="take attendance"){
+          history.push("/take-attendance");
         }
-      }
-    })
-
-  },[])
+      },
+    });
+  }, []);
   return (
-    <Router>
-      <Route path="/" exact component={HomePage} />
-      <Route path="/dashboard" exact component={DashboardPage} />
-      <Route path="/take-attendance"  component={TakeAttendancePage} />
-      <Route path="/admin"  component={AdminPage} />
-      <Route path="/login"  component={LoginPage} />
-      <Route path="/register"  component={RegisterPage} />
-      <Route path="/attendance"  component={DashboardPage} />
-    </Router>
-  )
-}
+    <>
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/dashboard" exact component={DashboardPage} />
+        <Route path="/take-attendance" component={TakeAttendancePage} />
+        <Route path="/admin" component={AdminPage} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/register" component={RegisterPage} />
+        <Route path="/attendance" component={DashboardPage} />
+      </Switch>
+    </>
+  );
+};
 
-export default App
+export default App;
