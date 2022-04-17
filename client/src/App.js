92794a15979574f6be62ac187bde61a5f503a50b
Hemanth Kumar <pusupalahemanthkumar@gmail.com>
2022-04-17 21:59:26 +0530

    logout voice-assistant

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import {Route, useHistory, Switch } from "react-router-dom";
+import { Route, useHistory, Switch } from "react-router-dom";
 import alanBtn from "@alan-ai/alan-sdk-web";
 
 import LoginPage from "./Screens/LoginPage";
@@ -8,29 +8,35 @@ import HomePage from "./Screens/HomePage";
 import TakeAttendancePage from "./Screens/TakeAttendancePage";
 import DashboardPage from "./Screens/DashboardPage";
 import AdminPage from "./Screens/AdminPage";
+import { useDispatch} from "react-redux";
+import { logout } from "../../actions/userActions";
 
 const ALAN_KEY =
   "032cb63c95e4a228e2cefa5e29d42e1b2e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 const App = () => {
   let history = useHistory();
+  const disptach = useDispatch();
   useEffect(() => {
     alanBtn({
       key: ALAN_KEY,
       onCommand: ({ command, pageName }) => {
         console.log(pageName);
         if (command === "home") {
-          history.push('/');
-        }else if(command==="dashboard"){
+          history.push("/");
+        } else if (command === "dashboard") {
           history.push("/dashboard");
-        }else if(command==="admin"){
+        } else if (command === "admin") {
           history.push("/admin/all");
-        }else if(command==="take attendance"){
+        } else if (command === "take attendance") {
           history.push("/take-attendance");
-        }else if(command==="login"){
+        } else if (command === "login") {
           history.push("/login");
-        }else if(command==="register"){
+        } else if (command === "register") {
           history.push("/register");
+        } else if (command === "logout") {
+          disptach(logout());
+          history.replace("/");
         }
       },
     });
